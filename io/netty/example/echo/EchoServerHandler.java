c81c656999c50a516fa121185f8889d4941e6943
taowy <815428229@qq.com>
2017-08-09 15:29:56 +0800

    add src

---
+++ io/netty/example/echo/EchoServerHandler.java
@@ -0,0 +1,22 @@
+package io.netty.example.echo;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+	@Override
+	      public void channelRead(ChannelHandlerContext ctx, Object msg) {
+	         ctx.writeAndFlush(msg);
+	      }
+	  
+	      @Override
+	      public void channelReadComplete(ChannelHandlerContext ctx) {
+	         ctx.flush();
+	      }
+	  
+	      @Override
+	      public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+	          // Close the connection when an exception is raised.
+	          cause.printStackTrace();
+	          ctx.close();
+	     }
+}
